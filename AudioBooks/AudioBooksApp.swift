76a26ce7e83accac72f590b35a76836117c6f3c1
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-08-01 15:53:51 +0530

    Initial Commit

---
+++ AudioBooks/AudioBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBooksApp.swift
+//  AudioBooks
+//
+//  Created by MANAS VIJAYWARGIYA on 01/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
